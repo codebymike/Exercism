11da1322cfb955bdd9f419306ff3301d99ee8938
Mike@server <mikethomas84@gmail.com>
2018-08-21 09:39:27 +0100

    Grains refactored, maths fail

--- elm/grains/Grains.elm
+++ elm/grains/Grains.elm
@@ -1,11 +1,9 @@
 module Grains exposing (square)
 
-import List exposing (foldl, range)
-
 
 square : Int -> Maybe Int
 square n =
     if n > 0 then
-        Just (foldl (\x acc -> acc * 2) 1 (range 2 n))
+        Just (2 ^ (n - 1))
     else
         Nothing
